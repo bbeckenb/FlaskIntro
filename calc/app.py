3d061e6426215e72cf7f9a94d6f19364b7272adc
Bryce Beckenbach <brycebeckenbach@gmail.com>
2021-06-11 14:28:07 -0400

    completed math operations get and post request

--- calc/app.py
+++ calc/app.py
@@ -1,6 +1,6 @@
 # Put your app in here.
 from flask import Flask, request
-from operations import add
+from operations import *
 
 app = Flask(__name__)
 
@@ -22,13 +22,71 @@ def add_form():
 
 @app.route('/add', methods=["POST"])
 def perform_addition():
-    a = request.form["a"]
-    b = request.form["b"]
+    a = int(request.form["a"])
+    b = int(request.form["b"])
     print(request.form)
     return f"""
     <h1>Your addition result is {add(a,b)}</h1>
     """
 
+@app.route('/sub')
+def sub_form():
+    return """
+    <h1>Subtract a and b</h1> 
+    <form method="POST">
+        <input type='number' placeholder='a' name='a'/>
+        <input type='number' placeholder='b' name='b'/>
+        <button>Submit</button>
+    </form>
+    """
 
+@app.route('/sub', methods=["POST"])
+def subtraction_addition():
+    a = int(request.form["a"])
+    b = int(request.form["b"])
+    print(request.form)
+    return f"""
+    <h1>Your subtraction result is {sub(a,b)}</h1>
+    """
+
+@app.route('/mult')
+def mult_form():
+    return """
+    <h1>Multiply a and b</h1> 
+    <form method="POST">
+        <input type='number' placeholder='a' name='a'/>
+        <input type='number' placeholder='b' name='b'/>
+        <button>Submit</button>
+    </form>
+    """
+
+@app.route('/mult', methods=["POST"])
+def perform_multiplication():
+    a = int(request.form["a"])
+    b = int(request.form["b"])
+    print(request.form)
+    return f"""
+    <h1>Your multiplication result is {mult(a,b)}</h1>
+    """
+
+@app.route('/div')
+def div_form():
+    return """
+    <h1>Divide a and b</h1> 
+    <form method="POST">
+        <input type='number' placeholder='a' name='a'/>
+        <input type='number' placeholder='b' name='b'/>
+        <button>Submit</button>
+    </form>
+    """
+
+@app.route('/div', methods=["POST"])
+def perform_division():
+    a = int(request.form["a"])
+    b = int(request.form["b"])
+    print(request.form)
+    return f"""
+    <h1>Your addition result is {div(a,b)}</h1>
+    """
 
 
